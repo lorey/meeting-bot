a14656ea05161038ea1ceca8855298cd4a06b9c0
bwst <github@bwstolz.de>
2018-05-05 18:34:28 +0200

    read out data from event for today

--- GoogleCalendar/quickstart.py
+++ GoogleCalendar/quickstart.py
@@ -11,6 +11,7 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import datetime
+import json
 
 # Setup the Calendar API
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
@@ -22,19 +23,34 @@ if not creds or creds.invalid:
 service = build('calendar', 'v3', http=creds.authorize(Http()))
 
 # Call the Calendar API
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-tomorrow = datetime.day + 1
-print(tomorrow)
-print('Getting the upcoming 10 events')
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time 
+tmrrw = datetime.datetime.utcnow() + datetime.timedelta(1)  
+tomorrow = tmrrw.isoformat() + 'Z'
+# print(now)
+# print(tomorrow)
+# print('Getting the upcoming 10 events')
 
 
-# events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=tomorrow      
-#                                       maxResults=10, singleEvents=True,
-#                                       orderBy='startTime').execute()
-# events = events_result.get('items', [])
+events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=tomorrow,      
+                                      singleEvents=True,
+                                      orderBy='startTime').execute()
+events = events_result.get('items', [])
 
-# if not events:
-#     print('No upcoming events found.')
-# for event in events:
-#     start = event['start'].get('dateTime', event['start'].get('date'))
-#     print(start, event['summary'])
+if not events:
+    print('No upcoming events found.')
+for event in events:
+    summary = event['summary']
+    summary = summary.lower()
+    if 'meeting:' in summary:
+        if event['attendees']:
+            attendees = event['attendees']
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            
+            print(attendees)
+    
+    
+    # attendees = event['attendees']
+    # print(json.dumps(event, indent=2))
+    # attendees = event['attendees'].get('displayName')
+    # print(start, event['summary'])
+    # print(attendees)
