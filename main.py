31bbea82b04bfd555b01d2cde7253b0ae9d7ef91
mawalu <web@mawalabs.de>
2018-05-05 17:43:39 +0200

    Initial version of telegram bot

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import TelegramBot.bot as telegram
+import os
+
+
 def main():
     print('I\'m a bot')
+
+    # Start telegram bot
+    updater = telegram.main()
+    bot = updater.bot
+
+    # Sample message
+    bot.send_message(chat_id=os.environ["DEBUG_CHAT_ID"], text="Test message")
+
+    # Keep the process alive
+    updater.idle()
+
 
 if __name__ == '__main__':
     main()
