8419713824f58fc62df2ac1d727e2fba083ba92a
lorey <git@karllorey.com>
2018-05-05 19:02:54 +0200

    Fix telegram handler for plain text

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 
 import telegram
-from telegram.ext import Updater, CommandHandler, Filters
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 
 import config
 import meetingbot
@@ -45,7 +45,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", state.start))
     dp.add_handler(CommandHandler("help", state.help))
-    dp.add_handler(Filters.all, state.receive)
+    dp.add_handler(MessageHandler(Filters.all, state.receive))
 
     # log all errors
     dp.add_error_handler(error)
