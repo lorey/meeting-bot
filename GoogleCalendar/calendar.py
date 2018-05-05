b640c7a9a96977f73c8ca3420715255b9b0d2862
bwst <github@bwstolz.de>
2018-05-05 19:21:27 +0200

    anged avariable name

--- GoogleCalendar/calendar.py
+++ GoogleCalendar/calendar.py
@@ -21,7 +21,7 @@ def next_meeeting():
     service = setup(telegram_id=1234)
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time 
     next_min = datetime.datetime.utcnow() + datetime.timedelta(0,61) # nextMin is used for timeMax, which is exclusive
-    nextMin = next_meeeting.isoformat() + 'Z'
+    nextMin = next_min.isoformat() + 'Z'
     events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=nextMin,      
                                         singleEvents=True,
                                         orderBy='startTime').execute()
