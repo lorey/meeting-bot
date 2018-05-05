5c44a842ddcf2456cb00572e074186bf6a4743b1
lorey <git@karllorey.com>
2018-05-05 17:13:38 +0200

    Push notes to Hubspot

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import config
+import meetingbot
+
+
 def main():
     print('I\'m a bot')
+    h = meetingbot.crm.Hubspot(config.HUBSPOT_API_KEY)
+
+    email = input('Email:')
+    note = input('Note:')
+    result = h.push_note(email, note)
+    print(result)
+
 
 if __name__ == '__main__':
     main()
--- meetingbot/crm.py
+++ meetingbot/crm.py
@@ -1,7 +1,5 @@
 import requests
 
-import config
-
 
 class Hubspot(object):
     api_key = None
@@ -9,6 +7,36 @@ class Hubspot(object):
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def find_cantact_by_email(self, email):
-        r = requests.get('https://api.hubapi.com/contacts/v1/contact/email/%s/profile?hapikey=%s' % (email, self.api_key))
-        print(r.json())
+    def find_contact_by_email(self, email):
+        endpoint = 'https://api.hubapi.com/contacts/v1/contact/email/%s/profile?hapikey=%s'
+        r = requests.get(endpoint % (email, self.api_key))
+        return r.json()
+
+    def push_note(self, email, note):
+        contact = self.find_contact_by_email(email)
+
+        if contact['status'] == 'error':
+            raise RuntimeError('Contact not found: %s' % contact)
+
+        endpoint = 'https://api.hubapi.com/engagements/v1/engagements?hapikey=%s' % self.api_key
+
+        data = {
+            "engagement": {
+                "active": True,
+                # "ownerId": 1,
+                "type": "NOTE",
+                # "timestamp": 1409172644778
+            },
+            "associations": {
+                "contactIds": [contact['vid']],
+                "companyIds": [],
+                "dealIds": [],
+                "ownerIds": [],
+            },
+            "metadata": {
+                "body": note,
+            }
+        }
+
+        response = requests.post(endpoint, json=data)
+        return response.json()
