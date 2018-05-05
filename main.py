77d836fdd158ef865487daf3b98ee8d64fef02b6
lorey <git@karllorey.com>
2018-05-05 18:27:33 +0200

    Queuing meeting notifications

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import config
 import meetingbot
 import TelegramBot.bot as telegram
@@ -17,10 +19,22 @@ def main():
     bot = updater.bot
 
     # Sample message
-    bot.send_message(chat_id=config["DEBUG_CHAT_ID"], text="Test message")
+    bot.send_message(chat_id=config.DEBUG_CHAT_ID, text="Test message")
 
     # Keep the process alive
     updater.idle()
+
+    # schedule calendar checks
+    interval = datetime.timedelta(seconds=60)
+    updater.job_queue.run_repeating(meeting_notifier, interval, first=0)
+
+
+def meeting_notifier(bot, job):
+    # todo make sure that there is no current dialogue
+    # todo fetch events
+    # todo check if event is ending within next minute
+    # todo open new dialogue that asks if you want to create a note
+    bot.send_message(chat_id=config.DEBUG_CHAT_ID, text='Checking your calendar')
 
 
 if __name__ == '__main__':
